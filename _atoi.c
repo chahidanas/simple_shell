bc3b178bbe3a39769a036fba719117998620187f
chahidanas <chahidanas27@gmail.com>
2024-01-05 09:25:19 +0100

    check 4

--- _atoi.c
+++ _atoi.c
@@ -33,11 +33,11 @@ int is_delim(char c, char *delim){
  *Return: 1 if c is alphabetic, 0 otherwise
  */
 int _isalpha(int c){
-	if ((c <= 'a' || c >= 'z') && (c <= 'A' || c >= 'Z')){
-		return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+		return (1);
 	}
 	else{
-		return (1);
+		return (0);
 	}
 }
 
